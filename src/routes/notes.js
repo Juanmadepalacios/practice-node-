df002bd279cc0f32a457cda7afbc143897e8f2ff
Juan Manuel de Palacios <juanma36@gmail.com>
2020-06-07 13:02:01 -0400

    validation errors

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.get('/notes/add', (req, res) =>{
+    res.render('notes/new-note');
+})
+
+router.post('/notes/new-note', (req, res) => {
+    //console.log(req.body);
+   const { title, description } = req.body;
+   const errors = [];
+   if(!title) {
+       errors.push({text: ' Please write a Title'});
+   }
+   if(!description) {
+       errors.push({text: ' Please write a Description'});
+   }
+    res.send('ok');
+})
+
+
+
 router.get('/notes', (req, res) =>{
     res.send('Notas desde nuestra base de datos');
 });
