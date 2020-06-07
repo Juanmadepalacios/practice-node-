5b40e49de340e1d9eface4e6a4fb05ccc80bbacc
Juan Manuel de Palacios <juanma36@gmail.com>
2020-06-07 13:16:14 -0400

    Validator error2

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,28 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new-note");
+});
 
-router.get('/notes/add', (req, res) =>{
-    res.render('notes/new-note');
-})
+router.post("/notes/new-note", (req, res) => {
+  //console.log(req.body);
+  const { title, description } = req.body;
+  const errors = [];
+  if (!title) {
+    errors.push({ text: "Please write a Title" });
+  }
+  if (!description) {
+    errors.push({ text: "Please write a Description" });
+  }
+  if (errors.length > 0) {
+    res.render("notes/new-note", {
+      errors,
+      title,
+      description,
+    });
+  } else {
+    res.send("ok");
+  }
+  
+});
 
-router.post('/notes/new-note', (req, res) => {
-    //console.log(req.body);
-   const { title, description } = req.body;
-   const errors = [];
-   if(!title) {
-       errors.push({text: ' Please write a Title'});
-   }
-   if(!description) {
-       errors.push({text: ' Please write a Description'});
-   }
-    res.send('ok');
-})
-
-
-
-router.get('/notes', (req, res) =>{
-    res.send('Notas desde nuestra base de datos');
+router.get("/notes", (req, res) => {
+  res.send("Notas desde nuestra base de datos");
 });
 
 module.exports = router;
